ade22c5d10ae2c6c0bdb87e973d12a5fa0d1ac23
aylingabaldon <aylingabaldon@gmail.com>
2025-10-29 09:42:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,11 @@ if st.button('Click me'):
   st.session_state['count'] += 1
 
 name = st.text_input("Escribe tu nombre")
-# st.write(nombre) 
-
+st.write(nombre) 
 st.write(st.session_state)
+
+
+
 # Con este código solo se pone el botón pero no incrementa el número
 # count = 0
 
