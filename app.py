af5d38efb0d290d89ad2441c1d80377cb28f8af9
aylingabaldon <aylingabaldon@gmail.com>
2025-10-29 09:30:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if 'count' not in st.session_state:
 # Con el código de arriba se va a agregar el count a la libreria de session state y tendrá el valor indicado
 
 if 'name' not in st.session_state:
-  st.session_state['name'] = ' '
+  st.session_state['name'] += ' '
   
 if st.button('Click me'):
   st.session_state['count'] += 1
